6b054e90943068c9c44e17cec6dcb0c308043224
Xebeth <xebeth@north-edge.com>
2019-12-14 04:30:04 +0100

    First version

---
+++ GeoscapeFontSize/GeoscapeFontSize.cs
@@ -0,0 +1,55 @@
+﻿using PhoenixPoint.Geoscape.View.ViewControllers;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using UnityEngine;
+using System.IO;
+using Harmony;
+using System;
+
+namespace PhoenixPointGeoscapeFontSize
+{
+    public class GeoscapeFontSize
+    {
+        public static int FONT_SIZE { get; set; } = 20;
+
+        public static void Init()
+        {
+            var harmony = HarmonyInstance.Create("io.github.xebeth.geoscapefontsize");
+            var optionFile = @"Mods/GeoscapeFontSize.json";
+
+            if (File.Exists(optionFile))
+            {
+                var Options = JObject.Parse(File.ReadAllText(optionFile));
+
+                if (int.TryParse((string)Options["size"], out int fontSize))
+                {
+                    FONT_SIZE = fontSize;
+                }
+            }
+            else
+            {
+                var Options = new JObject(new JProperty("size", FONT_SIZE));
+
+                File.WriteAllText(optionFile, Options.ToString());
+
+            }
+
+            harmony.PatchAll(Assembly.GetExecutingAssembly());            
+        }
+
+        [HarmonyPatch(typeof(GeoObjectiveElementController), "SetObjective")]
+        public static class PhoenixPoint_GeoObjectiveElementController_SetObjective_Patch
+        {
+            // void SetObjective(Sprite icon, Color iconColor, string objectiveText, int objectiveIndex)
+            static bool Prefix(GeoObjectiveElementController __instance, Sprite icon, Color iconColor, string objectiveText, int objectiveIndex)
+            {
+                if (__instance.ObjectiveText != null)
+                {
+                    __instance.ObjectiveText.fontSize = FONT_SIZE;
+                }
+
+                return true;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# GeoscapeFontSize
-Phoenix Point : Geoscape font size mod
+# Geoscape font size mod for Phoenix Point
+
+Modifies the font size of the objectives on the Geoscape (20 by default).
+Let the mod generate the configuration file or create a file named `GeoscapeFontSize.json` alongside the DLL containing the following JSON:
+```
+{
+  "size": 20
+}
+```
+
+Requires the Phoenix Point Mod Injector: https://github.com/RealityMachina/PhoenixPointModInjector
+
+Based on https://github.com/pardeike/Harmony
+<p align="center">
+	<img src="https://raw.githubusercontent.com/pardeike/Harmony/master/HarmonyLogo.png" alt="Harmony" width="128" />
+</p>
